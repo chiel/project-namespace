e0d926ca9c92fcaf4120867fe433e2d082c94d46
Chiel Kunkels <chiel@kunkels.me>
2021-08-03 01:15:55 +0200

    create service account

--- main.tf
+++ main.tf
@@ -8,3 +8,17 @@ resource "kubernetes_namespace" "this" {
     }
   }
 }
+
+resource "kubernetes_service_account" "deployer" {
+  metadata {
+    name      = "deployer"
+    namespace = kubernetes_namespace.this.metadata[0].name
+
+    labels = {
+      "app.kubernetes.io/managed-by" : "terraform"
+      "app.kubernetes.io/name" : "deployer"
+    }
+  }
+
+  automount_service_account_token = false
+}
